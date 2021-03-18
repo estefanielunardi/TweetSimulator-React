2628bdb616ef439063b59bc1a7fe69d20d41b8e9
Eni <estefanielunardi@users.noreply.github.com>
2021-03-18 19:42:13 +0100

    Tweet: List of Tweets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { Container, Snackbar } from '@material-ui/core'
+import React, {useState, useEffect } from 'react';
+import { Container, Snackbar } from '@material-ui/core/'
 import { Header } from './Components/Header/header'
 import { SendTweet } from './Components/SendTweet/SendTweet'
+import { TWEETS_STORAGE } from './utils/constants';
 
 import './App.scss';
 
 function App() {
+
+  const [ toastProps, setToastProps ] = useState({
+    open: false,
+    text: null
+  });
+
+  const [ allTweets, setAllTweets ] = useState([]); 
+
+  useEffect(() => {
+    const allTweetsStorage = localStorage.getItem(TWEETS_STORAGE);
+    const allTweetsArray = JSON.parse(allTweetsStorage);
+    setAllTweets(allTweetsArray);
+  }, []);
+
+  console.log(allTweets);
+
   return (
-      <Container className="tweets-simulator" maxWidth={false}>
-        <Header/>
-        <SendTweet />
-      </Container>
+    <Container className="tweets-simulator" maxWidth={false}>
+      <Header />
+      <SendTweet />
+      <SendTweet setToastProps={setToastProps} allTweets={allTweets}/>
+
+      <Snackbar 
+        anchorOrigin={{
+          vertical:"top",
+          horizontal:"right"
+        }}
+        open={toastProps.open}
+        //hacer el on close
+        autoHideDuration={1000}
+        message={<span id="message-id">{toastProps.text}</span>}
+      />
+    </Container>
   );
 }
 
